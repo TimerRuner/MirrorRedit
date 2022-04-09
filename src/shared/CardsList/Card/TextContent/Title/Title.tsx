9625227496a17dc482ec4b9b226d7f1fbff5a3f4
practice-stack1 <vadim3d2011@gmail.com>
2022-04-09 19:09:42 +0300

    made modal post

--- src/shared/CardsList/Card/TextContent/Title/Title.tsx
+++ src/shared/CardsList/Card/TextContent/Title/Title.tsx
@@ -1,12 +1,26 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "./title.css"
+import { Post } from "../../../../Post"
 
 export function Title() {
+    const [isModalOpened, setIsModalOpened] = useState(false)
+
     return (
         <h2 className={styles.title}>
-            <a href="#post-url" className={styles.postLink}>
+            <a
+                href="#post-url"
+                className={styles.postLink}
+                onClick={() => setIsModalOpened(true)}
+            >
                 Следует отметить, что новая модель организационной...
             </a>
+            {isModalOpened && (
+                <Post
+                    onClose={() => {
+                        setIsModalOpened(false)
+                    }}
+                />
+            )}
         </h2>
     )
 }
---
+++ src/shared/CommentForm/CommentForm.tsx
@@ -0,0 +1,29 @@
+import React, { ChangeEvent, FormEvent, useContext, useState } from "react"
+import { commentContext } from "../context/commentContext"
+import styles from "./commentform.css"
+
+export function CommentForm() {
+    const { value, onChange } = useContext(commentContext)
+
+    function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
+        onChange(event.target.value)
+    }
+
+    function handleSubmit(event: FormEvent) {
+        event.preventDefault()
+        console.log(value)
+    }
+
+    return (
+        <form className={styles.form} onSubmit={handleSubmit}>
+            <textarea
+                className={styles.input}
+                value={value}
+                onChange={handleChange}
+            ></textarea>
+            <button type="submit" className={styles.button}>
+                Коментувати
+            </button>
+        </form>
+    )
+}
