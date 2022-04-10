ca6c47dcb7bbe551ca0a05b9bdc99e06c59ff646
practice-stack1 <vadim3d2011@gmail.com>
2022-04-10 23:36:03 +0300

    add compression helmet delete hmr

--- src/server/server.js
+++ src/server/server.js
@@ -3,10 +3,18 @@ import ReactDOM from "react-dom/server"
 import { App } from "../App"
 import { indexTemplate } from "./indexTemplate"
 import axios from "axios"
+import compression from "compression"
+import helmet from "helmet"
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
+app.use(compression())
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+    })
+)
 
 app.use("/static", express.static("./dist/client"))
 
